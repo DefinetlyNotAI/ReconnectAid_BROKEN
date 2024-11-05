6ae24fd24681b5a2f4aab387a261056722383385
DefinetlyNotAI <Nirt_12023@outlook.com>
2024-11-05 14:09:00 +0400

    init

---
+++ reconnectaid/src/main/java/com/definetlynotai/reconnectaid/ReconnectaidApplication.java
@@ -0,0 +1,13 @@
+package com.definetlynotai.reconnectaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconnectaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconnectaidApplication.class, args);
+	}
+
+}
---
+++ reconnectaid/src/test/java/com/definetlynotai/reconnectaid/ReconnectaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.definetlynotai.reconnectaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconnectaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
